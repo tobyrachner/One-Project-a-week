509d3d6728cc047fd2c7dd61b3b35fd2dc4659f6
Toby Rachner <toby@rachner.com>
2024-02-14 15:48:13 -0600

    added /create route and ability to save images

--- website/views.py
+++ website/views.py
@@ -1,7 +1,27 @@
-from flask import Blueprint, render_template
+import os, sys
+from flask import Blueprint, render_template, request, send_from_directory, redirect, url_for, current_app
+from werkzeug.utils import secure_filename
+
+
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('home.html')
+
+@views.route('/create', methods=['GET', 'POST'])
+def create_project():
+    if request.method == 'POST':
+        file = request.files['img']
+        
+        filename = secure_filename(file.filename)
+        file.save(current_app.config['UPLOAD_FOLDER'] + filename)
+        return redirect(url_for('views.view_images', name=filename))
+
+    return render_template('create.html')
+
+@views.route('/images/<name>')
+def view_images(name):
+    return send_from_directory(current_app.config['UPLOAD_FOLDER'], name)
