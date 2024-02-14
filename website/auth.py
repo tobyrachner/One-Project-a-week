cf05350665740fa49154c03558d574782b6f9b65
Toby Rachner <toby@rachner.com>
2024-02-13 21:03:05 -0600

    added basic view structure & html + css for home page

---
+++ website/auth.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('login')
+def login():
+    return "<h1>login</h1>"
+
+@auth.route('logout')
+def logout():
+    return "<h1>logout</h1>"
